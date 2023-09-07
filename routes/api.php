c38c6c83e19011faf9b588686f4da2033331786e
drozdiam <drozdovskiy_ilya@mail.ru>
2023-09-07 16:43:08 +0300

    add api routs

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'categories' => CategoryController::class,
+    'users' => UserController::class,
+    'articles' => ArticleController::class,
+]);
