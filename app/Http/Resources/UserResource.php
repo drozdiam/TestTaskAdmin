5be5b2dd5602990ccfafda1a09c3ef437172f05e
drozdiam <drozdovskiy_ilya@mail.ru>
2023-09-07 16:57:29 +0300

    edit UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,13 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'active' => $this->active,
+            'email_verified_at' => $this->email_verified_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
     }
 }
