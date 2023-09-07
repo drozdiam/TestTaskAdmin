c0330a436d10fd75cd7f12c19fb2239c9b87920f
drozdiam <drozdovskiy_ilya@mail.ru>
2023-09-07 16:44:16 +0300

    edit user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,15 +14,17 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        static $i = 0;
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => password_hash('123456789', PASSWORD_BCRYPT),
             'remember_token' => Str::random(10),
+            'order' => $i++,
+            'active' => true,
         ];
     }
-
     /**
      * Indicate that the model's email address should be unverified.
      *
