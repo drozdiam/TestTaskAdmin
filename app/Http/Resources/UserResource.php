6d14470056adaf6f75406d8ca32c2f259ad19808
drozdiam <drozdovskiy_ilya@mail.ru>
2023-09-11 00:46:07 +0300

    add order,id UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,9 +15,11 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
             'active' => $this->active,
+            'order' => $this->order,
             'email_verified_at' => $this->email_verified_at,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at
