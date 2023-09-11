4bdc4137e7a2199f1558f1a079924ac0ce9850a5
drozdiam <drozdovskiy_ilya@mail.ru>
2023-09-11 03:32:13 +0300

    add migration articles

---
+++ database/migrations/2023_09_07_115035_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->foreignId('category_id');
+            $table->string('image')->nullable();
+            $table->text('text')->nullable();
+            $table->boolean('active')->default(true);
+            $table->integer('order')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
